953837ab051583dcc0f8cf8426facd57d1f9ab51
Mari Batilando <mari@weebly.com>
2015-02-11 15:28:09 -0800

    Start FiltersViewController

---
+++ Yelp/FiltersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FiltersViewController.swift
+//  Yelp
+//
+//  Created by Mari Batilando on 2/11/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import Cocoa
+
+class FiltersViewController: UIViewController {
+
+}
