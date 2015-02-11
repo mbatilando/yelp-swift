b5d08f599a6d75841e8983c89d81792022c989b7
Mari Batilando <mari@weebly.com>
2015-02-11 11:29:19 -0800

    Displaying Custom Cells work

--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var client: YelpClient!
     
+    @IBOutlet weak var resultsTableView: UITableView!
     // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
     let yelpConsumerKey = "DlBsYPjUKUE605DH45PWQQ"
     let yelpConsumerSecret = "cQlvZRMlfmJhTABcjCMTeRT6Ceg"
     let yelpToken = "MlVd2OpJvnVlszL8Wlk6q6lldu94lT-A"
     let yelpTokenSecret = "_8Zfs3B64WqA5nKqtxp_ta_n6cM"
-    var resultsDictionary: NSDictionary
+    var restaurants: NSArray?
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -24,12 +25,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.resultsTableView.dataSource = self
+        self.resultsTableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
         client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
         
         client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
             println(response)
-            self.resultsDictionary = response as NSDictionary
+            self.restaurants = response["businesses"] as NSArray
+            self.resultsTableView.reloadData()
         }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
             println(error)
         }
@@ -41,11 +45,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(<#identifier: String#>)
+        let cell = tableView.dequeueReusableCellWithIdentifier("RestaurantCell") as RestaurantCell
+        let business = self.restaurants![indexPath.row] as NSDictionary
+        let name = business["name"] as NSString
+        cell.restaurantNameLabel.text = name
+        let location = business["location"] as NSDictionary
+        let streetObj = location["address"] as NSArray
+//        let street = streetObj as
+        // TODO: Add neighborhood
+        cell.restaurantAddressLabel.text = streetObj[0] as NSString
+        return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if self.restaurants != nil {
+            return self.restaurants!.count
+        }
+        return 0
     }
     
     
