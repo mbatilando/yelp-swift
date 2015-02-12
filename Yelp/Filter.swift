f513f08e56ccabb72ccf7d627390331c9a3e9d8c
Mari Batilando <mari@weebly.com>
2015-02-12 13:11:32 -0800

    Add Filter Class

---
+++ Yelp/Filter.swift
@@ -0,0 +1,20 @@
+//
+//  Filter.swift
+//  Yelp
+//
+//  Created by Mari Batilando on 2/12/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+class Filter {
+    var label: String
+    var value: String
+    var active: Bool
+
+    init(label: String, value: String, active:Bool = false) {
+        self.label = label
+        self.value = value
+        self.active = active
+    }
+}
