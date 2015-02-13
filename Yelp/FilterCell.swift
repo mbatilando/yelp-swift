5569de5b446f98f4af07b64e7af051c358e668a4
Mari Batilando <mari@weebly.com>
2015-02-12 16:47:22 -0800

    Add Filter Cell

---
+++ Yelp/FilterCell.swift
@@ -0,0 +1,27 @@
+//
+//  FilterCell.swift
+//  Yelp
+//
+//  Created by Mari Batilando on 2/12/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UITableViewCell {
+
+    @IBOutlet weak var filterNameLabel: UILabel!
+    @IBOutlet weak var filterSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
